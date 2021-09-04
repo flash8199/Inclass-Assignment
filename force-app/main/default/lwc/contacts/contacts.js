e417aa1cabc23973c9674bd1536d602cabc6e58d
flash8199 <dasaradh.akaspu.8199@gmail.com>
2021-09-04 13:41:04 +0530

    First commit

--- force-app/main/default/lwc/contacts/contacts.js
+++ force-app/main/default/lwc/contacts/contacts.js
@@ -1,8 +1,29 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, wire} from 'lwc';
+import searchContacts from '@salesforce/apex/contactController.searchContacts';
 
 export default class Contacts extends LightningElement {
 
     @track searchValue;
+
+
+    @wire(searchContacts) wiredContacts;
+    getContacts() {
+        searchContacts()
+        .then(contacts => {
+            console.log('Got Contacts: ' + contacts.length);
+        })
+        .catch(error => {
+            console.log(error)
+        });
+    }
+    value = '';
+
+    get options() {
+        return [
+            { label: 'All Contacts', value: 'option1' },
+            { label: 'Search Pattern', value: 'option2' },
+        ];
+    }
 
     handleChange(event){
         const value = event.target.value;
